0a591820bd2f6579db3994940073d1a8b1a1a2bf
Anastasiia Bondar <nastya.v.bondar@gmail.com>
2016-10-15 16:26:32 +0300

    Add some news link

--- js/companies-view.js
+++ js/companies-view.js
@@ -9,9 +9,113 @@ $(function() {
                 for (var i = 0; i < list.length; i++) {
                     var $item = $("<li>");
                     $item.appendTo(".companies-list");
-                    $item.text(list[i]['name']);
+                    var $link = $("<a>");
+                    $link.text(list[i]['name']);
+                    $link.attr('href', list[i]['partners']);
+                    $link.appendTo($item);
+
+
                 }
             });
     }
+
+    function getNews() {
+        $.getJSON('http://codeit.pro/frontTestTask/news/getList',
+            function(resp) {
+                console.log('resp', resp);
+                var catalog = resp["list"];
+                $(".carousel-indicators").empty();
+                for (var i = 0; i < catalog.length; i++) {
+                    var $indicator = $("<li>");
+                    $indicator.attr('data-target', '#myCarousel');
+                    $indicator.attr('data-slide-to', i);
+                    if (i === 0) {
+                        $indicator.addClass("active");
+                    }
+                    $indicator.appendTo(".carousel-indicators");
+                };
+
+                $(".carousel-inner").empty();
+                for (var i = 0; i < catalog.length; i++) {
+                    var $item = $("<div>");
+                    $item.addClass("item");
+                    if (i === 0) {
+                        $item.addClass("active");
+                    }
+                    $item.appendTo(".carousel-inner");
+
+
+                    var $content = $("<div>");
+                    $content.addClass("form-content news");
+                    $content.appendTo($item);
+
+                    // add block short info
+                    var $shortInfo = $("<div>");
+                    $shortInfo.addClass("short-info");
+                    $shortInfo.appendTo($content);
+
+                    // add img
+                    var $newsImage = $("<div>");
+                    $newsImage.addClass("news-image");
+                    $newsImage.appendTo($shortInfo);
+
+                    var $image = $("<img>");
+                    $image.attr("src", catalog[i]['img']);
+                    $image.attr("alt", catalog[i]['author']);
+                    $image.appendTo($newsImage);
+
+                    // add author
+                    var $author = $("<div>");
+                    $author.addClass("author")
+                    $author.appendTo($shortInfo);
+
+                    var $bold = $("<b>");
+                    $bold.text("Author : ");
+                    $bold.appendTo($author)
+
+                    var $authorName = $("<span>");
+                    $authorName.text(catalog[i]['author']);
+                    $authorName.appendTo($author);
+                    // add date
+                    var $public = $("<div>");
+                    $public.addClass("public");
+                    $public.appendTo($shortInfo);
+
+                    var $boldDate = $("<b>");
+                    $boldDate.text("Public : ");
+                    $boldDate.appendTo($public);
+
+
+                    var $publicDate = $("<span>");
+                    $publicDate.text(catalog[i]['date']);
+                    $publicDate.appendTo($public);
+
+                    // add block short content
+
+                    var $shortContent = $("<div>");
+                    $shortContent.addClass("short-content");
+                    $shortContent.appendTo($content);
+
+                    // add title
+                    var $title = $("<a>");
+                    $title.addClass("link-title");
+                    $title.text(catalog[i]['author']);
+                    $title.attr('href', catalog[i]['link']);
+                    $title.appendTo($shortContent);
+
+                    // add description
+                    var $description = $("<p>");
+                    $description.text(catalog[i]['description']);
+                    $description.appendTo($shortContent);
+
+                }
+
+            });
+
+    }
+
+
+
+    getNews();
     getList();
 });
